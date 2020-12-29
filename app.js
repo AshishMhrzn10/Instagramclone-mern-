f4bf5b36781635a633fd89fb728f74c86ff39085
Ashish Maharjan <amaharjan1032@gmail.com>
2020-12-29 13:22:45 +0545

    mongodb setup

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 5000;
+const { MONGOURI } = require("./keys");
 
-app.get("/", (req, res) => {
-	res.send("Hello world");
+mongoose.connect(MONGOURI, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connection.on("connected", () => {
+	console.log("Connected to mongodb");
+});
+
+mongoose.connection.on("error", (err) => {
+	console.log("Error connecting", err);
 });
 
 app.listen(PORT, () => {
