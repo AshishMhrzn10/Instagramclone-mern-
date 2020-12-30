d4ad3a78aca595768cc73770b47f32b746e4d883
Ashish Maharjan <amaharjan1032@gmail.com>
2020-12-30 13:00:38 +0545

    saving data in mongoose

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-	res.send("hello");
-});
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
 
 router.post("/signup", (req, res) => {
 	const { name, email, password } = req.body;
 	if (!email || !password || !name) {
 		return res.status(422).json({ error: "Please add all the fields" });
-	} else {
-		res.json({ message: "successfully posted" });
 	}
+	User.findOne({ email: email })
+		.then((savedUser) => {
+			if (savedUser) {
+				return res.status(422).json({ error: "Email already exists" });
+			}
+			bcrypt.hash(password, 12).then((hashedpassword) => {
+				const user = new User({
+					email,
+					password: hashedpassword,
+					name,
+				});
+				user
+					.save()
+					.then((user) => {
+						res.json({ message: "saved successfully" });
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 module.exports = router;
