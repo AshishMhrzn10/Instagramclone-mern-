7b9aa0f526514320754024ff38a4e04e7b8877f8
Ashish Maharjan <amaharjan1032@gmail.com>
2021-01-05 09:17:31 +0545

    Home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,63 @@
 import React from "react";
 
 const Home = () => {
-	return <h1>Home</h1>;
+	return (
+		<div className="home">
+			<div className="card home-card">
+				<h5>John</h5>
+				<div className="card-image">
+					<img
+						src="https://images.unsplash.com/photo-1554147090-e1221a04a025?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+						alt=""
+					/>
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{ color: "red" }}>
+						favorite
+					</i>
+					<h6>Title</h6>
+					<p>This is amazing post</p>
+					<input type="text" placeholder="Add a comment.." />
+				</div>
+			</div>
+
+			<div className="card home-card">
+				<h5>John</h5>
+				<div className="card-image">
+					<img
+						src="https://images.unsplash.com/photo-1554147090-e1221a04a025?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+						alt=""
+					/>
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{ color: "red" }}>
+						favorite
+					</i>
+					<h6>Title</h6>
+					<p>This is amazing post</p>
+					<input type="text" placeholder="Add a comment.." />
+				</div>
+			</div>
+
+			<div className="card home-card">
+				<h5>John</h5>
+				<div className="card-image">
+					<img
+						src="https://images.unsplash.com/photo-1554147090-e1221a04a025?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdhbGxwYXBlcnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+						alt=""
+					/>
+				</div>
+				<div className="card-content">
+					<i class="material-icons" style={{ color: "red" }}>
+						favorite
+					</i>
+					<h6>Title</h6>
+					<p>This is amazing post</p>
+					<input type="text" placeholder="Add a comment.." />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
