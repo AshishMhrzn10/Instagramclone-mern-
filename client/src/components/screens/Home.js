1c9155fcdbf7d653600dcd10878d0eaf58567ecb
Ashish Maharjan <amaharjan1032@gmail.com>
2021-01-18 07:51:30 +0545

    like unlike

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../App";
 
 const Home = () => {
 	const [data, setData] = useState([]);
+	const { state, dispatch } = useContext(UserContext);
 
 	useEffect(() => {
 		fetch("/allpost", {
@@ -14,6 +16,60 @@ const Home = () => {
 				setData(result.posts);
 			});
 	}, []);
+
+	const likePost = (id) => {
+		fetch("/like", {
+			method: "put",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + localStorage.getItem("jwt"),
+			},
+			body: JSON.stringify({
+				postId: id,
+			}),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				const newData = data.map((item) => {
+					if (item._id === result._id) {
+						return result;
+					} else {
+						return item;
+					}
+				});
+				setData(newData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const unlikePost = (id) => {
+		fetch("/unlike", {
+			method: "put",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + localStorage.getItem("jwt"),
+			},
+			body: JSON.stringify({
+				postId: id,
+			}),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				const newData = data.map((item) => {
+					if (item._id === result._id) {
+						return result;
+					} else {
+						return item;
+					}
+				});
+				setData(newData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
 	return (
 		<div className="home">
@@ -28,6 +84,23 @@ const Home = () => {
 							<i className="material-icons" style={{ color: "red" }}>
 								favorite
 							</i>
+							{item.likes.includes(state._id) ? (
+								<i
+									className="material-icons"
+									onClick={() => unlikePost(item._id)}
+								>
+									thumb_down
+								</i>
+							) : (
+								<i
+									className="material-icons"
+									onClick={() => likePost(item._id)}
+								>
+									thumb_up
+								</i>
+							)}
+
+							<h6>{item.likes.length} likes</h6>
 							<h6>{item.title}</h6>
 							<p>{item.body}</p>
 							<input type="text" placeholder="Add a comment.." />
