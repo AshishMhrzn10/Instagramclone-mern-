11625cafbc137408768e02b3051e89f9691f6abf
Ashish Maharjan <amaharjan1032@gmail.com>
2021-02-03 13:03:58 +0545

    Changes updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = 5000;
-const { MONGOURI } = require("./keys");
+const PORT = process.env.PORT || 5000;
+const { MONGOURI } = require("./config/keys");
 
 mongoose.connect(MONGOURI, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.connection.on("connected", () => {
@@ -20,6 +20,14 @@ app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log("Server is running on", PORT);
