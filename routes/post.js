950b2f84bd85598fa3e49502ed2845cf8fff9d54
Ashish Maharjan <amaharjan1032@gmail.com>
2021-02-08 13:02:59 +0545

    show latest post on top

--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ router.get("/allpost", requireLogin, (req, res) => {
 	Post.find()
 		.populate("postedBy", "_id name")
 		.populate("comments.postedBy", "_id name")
+		.sort("-createdAt")
 		.then((posts) => {
 			res.json({ posts });
 		})
@@ -20,6 +21,7 @@ router.get("/getsubpost", requireLogin, (req, res) => {
 	Post.find({ postedBy: { $in: req.user.following } })
 		.populate("postedBy", "_id name")
 		.populate("comments.postedBy", "_id name")
+		.sort("-createdAt")
 		.then((posts) => {
 			res.json({ posts });
 		})
