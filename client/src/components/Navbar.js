16b420e5a423b5ea4dc208fd7f6bfe759367b89e
Ashish Maharjan <amaharjan1032@gmail.com>
2021-02-08 13:25:36 +0545

    search user frontend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../App";
+import M from "materialize-css";
 
 const NavBar = () => {
+	const searchModal = useRef(null);
+	const [search, setSearch] = useState("");
 	const { state, dispatch } = useContext(UserContext);
 	const history = useHistory();
+
+	useEffect(() => {
+		M.Modal.init(searchModal.current);
+	}, []);
+
 	const renderList = () => {
 		if (state) {
 			return [
-				<li>
+				<li key="1">
+					<i
+						data-target="modal1"
+						className="large material-icons modal-trigger"
+						style={{ color: "black" }}
+					>
+						search
+					</i>
+				</li>,
+
+				<li key="2">
 					<Link to="/profile">Profile</Link>
 				</li>,
-				<li>
+				<li key="3">
 					<Link to="/create">Create post</Link>
 				</li>,
-				<li>
+				<li key="4">
 					<Link to="/myfollowingpost">My following post</Link>
 				</li>,
-				<button
-					className="btn #c62828 red darken-3"
-					onClick={() => {
-						localStorage.clear();
-						dispatch({
-							type: "CLEAR",
-						});
-						history.push("/signin");
-					}}
-				>
-					Logout
-				</button>,
+				<li key="5">
+					<button
+						className="btn #c62828 red darken-3"
+						onClick={() => {
+							localStorage.clear();
+							dispatch({
+								type: "CLEAR",
+							});
+							history.push("/signin");
+						}}
+					>
+						Logout
+					</button>
+				</li>,
 			];
 		} else {
 			return [
-				<li>
+				<li key="6">
 					<Link to="/signin">Signin</Link>
 				</li>,
-				<li>
+				<li key="7">
 					<Link to="/signup">Signup</Link>
 				</li>,
 			];
@@ -52,6 +72,33 @@ const NavBar = () => {
 					<ul id="nav-mobile" className="right">
 						{renderList()}
 					</ul>
+				</div>
+
+				<div
+					id="modal1"
+					className="modal"
+					ref={searchModal}
+					style={{ color: "black" }}
+				>
+					<div className="modal-content">
+						<input
+							type="text"
+							placeholder="Search user"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+						<ul className="collection" style={{ display: "grid" }}>
+							<li className="collection-item">Alvin</li>
+							<li className="collection-item">Alvin</li>
+							<li className="collection-item">Alvin</li>
+							<li className="collection-item">Alvin</li>
+						</ul>
+					</div>
+					<div className="modal-footer">
+						<button className="modal-close waves-effect waves-green btn-flat">
+							Agree
+						</button>
+					</div>
 				</div>
 			</nav>
 		</div>
